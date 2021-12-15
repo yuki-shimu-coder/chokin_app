30dd585895d18ba69fb158e5f0cf0a2b44be9a41
github yuki-shimu-coder <yuyuyou7@gmail.com>
2021-12-16 07:36:03 +0900

    headerの編集

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,16 +1,32 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>新規登録</title>
 </head>
+
 <body>
   <header class="l-header">
     <div class="l-container">
-      
+      <div class="p-header">
+        <div class="p-header__logo">
+          <a href="">
+            <img src="" alt="">
+            Chokin
+          </a>
+        </div>
+        <nav class="p-header__nav">
+          <ul class="p-header__menu">
+            <li class="p-header__menu-item">ログイン</li>
+            <li class="p-header__menu-item">新規登録</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   </header>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('signup',function(){
+    return view('signup');
+});
