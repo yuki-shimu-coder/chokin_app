3a0163134cacb394ff93617894f9d7fc7274e740
github yuki-shimu-coder <yuyuyou7@gmail.com>
2021-12-19 14:13:29 +0900

    メディアクエリのsass変数の参照及びスマホレスポンシブデザインの設定

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+    integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
   <title>新規登録</title>
 </head>
 
@@ -25,6 +27,7 @@
             <li class="p-header__menu-item"><a href="">新規登録</a></li>
           </ul>
         </nav>
+        <i class="fas fa-bars"></i>
       </div>
     </div>
   </header>
