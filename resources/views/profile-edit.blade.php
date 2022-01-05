bfa9ecdf110e1d8c1f4339bb63a6649454259d03
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-05 14:49:00 +0900

    プロフィール編集画面用のscssファイルを作成

--- resources/views/profile-edit.blade.php
+++ resources/views/profile-edit.blade.php
@@ -4,4 +4,17 @@
 
 @section('content')
 
+<div class="l-container">
+  <div class="p-profile">
+    <div class="p-profile__side">
+
+    </div>
+    <div class="p-profile__mail">
+
+    </div>
+
+  </div>
+
+</div>
+
 @endsection
