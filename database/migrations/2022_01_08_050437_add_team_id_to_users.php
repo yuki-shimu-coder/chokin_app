ea30473fdf04810abfe16e6de6d21dae5f0e9600
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-08 14:10:47 +0900

    team_idカラム追加用のマイグレーションファイルを編集
    passwordカラムの後ろに追加されるように変更

--- database/migrations/2022_01_08_050437_add_team_id_to_users.php
+++ database/migrations/2022_01_08_050437_add_team_id_to_users.php
@@ -14,7 +14,12 @@ class AddTeamIdToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            //passwordカラムのすぐ後ろに、team_idカラムを追加する
+            $table->unsignedBigInteger('team_id')->after('password');
+            // 外部キー制約の設定
+            $table->foreign('team_id')        // foreignメソッドでteam_idを外部キーに設定する。
+                ->references('id')            // referencesメソッドで、従テーブルのteam_idと紐付いている主テーブルのidを指定する。
+                ->on('teams');
         });
     }
 
@@ -26,7 +31,10 @@ class AddTeamIdToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            // 外部キー付きのカラムを削除するには、まず必ず外部キー制約を外す。
+            $table->dropForeign(['team_id']);
+            // 続いてカラムを削除する。
+            $table->dropColumn('team_id');
         });
     }
 }
