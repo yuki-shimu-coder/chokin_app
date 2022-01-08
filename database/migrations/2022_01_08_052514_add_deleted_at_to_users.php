64e15fb7299629298c26fa511cc9bc49bf55caac
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-08 14:32:58 +0900

    ソフトデリートのマイグレーションファイル完成

--- database/migrations/2022_01_08_052514_add_deleted_at_to_users.php
+++ database/migrations/2022_01_08_052514_add_deleted_at_to_users.php
@@ -14,7 +14,8 @@ class AddDeletedAtToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            //deleted_atカラムを追加する。このカラムは、ソフトデリート（論理削除）用として活用される。
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +27,8 @@ class AddDeletedAtToUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            //deleted_atカラムのドロップ
+            $table->dropSoftDeletes();
         });
     }
 }
