22cd8ef9e614029e1f723ed6ac5f82e9ecb3bf6c
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-09 12:03:28 +0900

    seederファイルの'team_name 'を'team_name'に修正

--- database/seeds/TeamsTableSeeder.php
+++ database/seeds/TeamsTableSeeder.php
@@ -16,40 +16,40 @@ class TeamsTableSeeder extends Seeder
         // 部署配列
         $data = array(
             array(
-                'team_name ' => '総務課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '総務課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '企画商工課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '企画商工課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '農林課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '農林課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '建設課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '建設課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '住民課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '住民課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '福祉子育て支援課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '福祉子育て支援課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => 'トマム支所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => 'トマム支所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '占冠保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '占冠保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => 'トマム保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => 'トマム保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '会計室', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '会計室', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '教育委員会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '教育委員会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
             array(
-                'team_name ' => '議会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+                'team_name' => '議会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
             ),
         );
         //teamsテーブルにデータを登録する
