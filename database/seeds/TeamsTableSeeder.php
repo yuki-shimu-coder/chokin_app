14bb2e3527bba1d34b5142a5a08c64e1eaef94e0
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-09 11:59:08 +0900

    teamsテーブルのseederファイル完成

--- database/seeds/TeamsTableSeeder.php
+++ database/seeds/TeamsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class TeamsTableSeeder extends Seeder
 {
@@ -11,6 +13,46 @@ class TeamsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 部署配列
+        $data = array(
+            array(
+                'team_name ' => '総務課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '企画商工課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '農林課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '建設課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '住民課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '福祉子育て支援課', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => 'トマム支所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '占冠保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => 'トマム保育所', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '会計室', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '教育委員会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+            array(
+                'team_name ' => '議会', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(),
+            ),
+        );
+        //teamsテーブルにデータを登録する
+        DB::table('teams')->insert($data);
     }
 }
