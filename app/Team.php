f35796c01784aec8f290712ccf31c8d2212eb041
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-09 17:18:27 +0900

    TeamモデルにUserモデルのリレーションを設定

--- app/Team.php
+++ app/Team.php
@@ -8,4 +8,10 @@ class Team extends Model
 {
     //$fillableの設定
     protected $fillable = ['team_name'];
+
+    // リレーションの設定
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
