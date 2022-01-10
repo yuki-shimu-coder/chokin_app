c277010a2880e24d7aa19716335ce8296963d450
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-10 14:34:14 +0900

    新規登録画面のビューを整理

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,112 +1,79 @@
-@extends('layouts.app')
+@extends('template_self.app')
+
+@section('title','新規登録')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        {{-- ユーザーネーム入力欄 --}}
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- email入力欄 --}}
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address')
-                                }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- password入力欄 --}}
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password')
-                                }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- password再入力欄 --}}
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm
-                                Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control"
-                                    name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        {{-- 所属部署選択欄 --}}
-                        <div class="form-group row">
-                            <label for="BelongsTeam" class="col-md-4 col-form-label text-md-right">{{ __('Belongs team')
-                                }}</label>
-
-                            <div class="col-md-6">
-
-                                <select name="team_id" id="BelongsTeam" class="form-control" required >
-
-                                    {{-- teamsテーブルから取得したデータを展開 --}}
-                                    @foreach ($teams as $team)  
-                                        <option value="{{$team->id}}">{{$team->team_name}}</option>
-                                    @endforeach
-                                </select>
-
-                                @error('team_id')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        {{-- 登録ボタン --}}
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="l-container">
+    <div class="p-signup">
+        <div class="p-signup__img-wrap u-mgr--60">
+            <div class="u-mgb--40">
+                <img src="{{asset('img/undraw_in_the_office_re_jtgc.svg')}}" alt="">
             </div>
+            <p>超勤をかんたん管理</p>
+        </div>
+        <div class="p-signup__form">
+            <h1 class="p-signup__title u-mgb--40">ユーザー登録</h1>
+            <form  method="POST" action="{{ route('register') }}">
+                @csrf
+
+                {{-- ユーザーネーム入力欄 --}}
+                <div id="c-input-username">
+                    <i class="fas fa-user c-input-icon"></i>
+                    <input id="name" name="name" type="text" class="c-auth-input @error('name') is-invalid @enderror"
+                        placeholder="名前を入力してください" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                    @error('name')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                {{-- email入力欄 --}}
+                <div id="c-input-email">
+                    <i class="fas fa-envelope c-input-icon"></i>
+                    <input id="email" type="email" class="c-auth-input @error('email') is-invalid @enderror"
+                        name="email" value="{{ old('email') }}" required autocomplete="email"
+                        placeholder="Emailアドレスを入力してください">
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                {{-- password入力欄 --}}
+                <div id="c-input-password">
+                    <i class="fas fa-lock c-input-icon"></i>
+                    <input id="password" type="password" class="c-auth-input @error('password') is-invalid @enderror"
+                        name="password" required autocomplete="new-password" type="password"
+                        placeholder="パスワードを入力してください">
+                </div>
+
+                {{-- password再入力欄 --}}
+                <div id="c-input-password-re">
+                    <i class="fas fa-lock c-input-icon"></i>
+                    <input id="password-confirm" type="password" class="c-auth-input" name="password_confirmation"
+                        required autocomplete="new-password" placeholder="もう一度パスワードを入力してください">
+                </div>
+
+                {{-- 所属部署選択欄--}}
+                <div id="c-input-teams">
+                    <i class="fas fa-address-card c-input-icon"></i>
+                    <select name="team_id" class="c-auth-input" required>
+                        {{-- teamsテーブルから取得したデータを展開 --}}
+                        @foreach ($teams as $team)
+                        <option value="{{$team->id}}">{{$team->team_name}}</option>
+                        @endforeach
+                    </select>
+                    @error('team_id')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <button type="submit" class="c-button p-signup__button">SIGN UP</button>
+            </form>
         </div>
     </div>
 </div>
