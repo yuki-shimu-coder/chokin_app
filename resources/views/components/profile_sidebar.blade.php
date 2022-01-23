0940b11aa35b163f29cd7d9c023ac93ca48b67f7
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-01-23 15:13:56 +0900

    プロフィール編集画面サイドバーのコンポーネント化
    >プロフィール編集画面、パスワード編集画面のサイドバーをコンポーネント化
    >コンポーネントにするサイドバーを切り取り、profile_sidebar.blade.phpを作成
    >それぞれの画面で以下を呼び出し
     @component('components.profile_sidebar')
     @endcomponent

---
+++ resources/views/components/profile_sidebar.blade.php
@@ -0,0 +1,7 @@
+<div class="p-profile__side">
+  <ul>
+    <li class="p-profile__list-item"><a href="{{route('profile-edit')}}">プロフィール編集</a></li>
+    <li class="p-profile__list-item"><a href="{{route('password-edit')}}">パスワード編集</a></li>
+    <li class="p-profile__list-item"><a href="">退会</a></li>
+  </ul>
+</div>
--- resources/views/template_self/profile-pass.blade.php
+++ resources/views/template_self/profile-pass.blade.php
@@ -8,13 +8,8 @@
   <div class="p-profile">
 
     {{-- サイドバー --}}
-    <div class="p-profile__side">
-      <ul>
-        <li class="p-profile__list-item"><a href="{{route('profile-edit')}}">プロフィール編集</a></li>
-        <li class="p-profile__list-item"><a href="{{route('password-edit')}}">パスワード編集</a></li>
-        <li class="p-profile__list-item"><a href="">退会</a></li>
-      </ul>
-    </div>
+    @component('components.profile_sidebar')
+    @endcomponent
 
     {{-- 編集メイン --}}
     <div class="p-profile__main">
