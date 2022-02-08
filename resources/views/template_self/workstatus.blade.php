990395628c8114bf1a82ea4a90396e76541a96d1
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-02-08 21:07:06 +0900

    不要になったhtml要素を削除

--- resources/views/template_self/workstatus.blade.php
+++ resources/views/template_self/workstatus.blade.php
@@ -45,56 +45,6 @@
         </div>
         @endforeach
 
-        {{-- <div class="p-work-status__history-contents">
-          <time class="p-work-status__history-date" datetime="2022-01-01">2022年1月1日</time>
-          <div class="p-work-status__history-worktime">
-            <span>3</span>時間
-            <div style="display:inline;margin-right: 5px;"></div>
-            <span>45</span>分
-          </div>
-          <div class="p-work-status__history-subject">
-            国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調
-          </div>
-          <div class="p-work-status__history-edit-trigger">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-          <div class="p-work-status__history-edit-wrap">
-
-            <i class="fas fa-times-circle p-work-status__history-edit-close"></i>
-            <div class="u-mgb--10">
-              <a href="" class="c-button --edit">編集</a>
-            </div>
-            <form action="" method="POST">
-              <button class="c-button --delete">削除</button>
-            </form>
-          </div>
-        </div> --}}
-
-        {{-- <div class="p-work-status__history-contents">
-          <time class="p-work-status__history-date" datetime="2022-01-01">2022年1月1日</time>
-          <div class="p-work-status__history-worktime">
-            <span>3</span>時間
-            <div style="display:inline;margin-right: 5px;"></div>
-            <span>45</span>分
-          </div>
-          <div class="p-work-status__history-subject">
-            国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調査集計用務のため国勢調
-          </div>
-          <div class="p-work-status__history-edit-trigger">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-          <div class="p-work-status__history-edit-wrap">
-
-            <i class="fas fa-times-circle p-work-status__history-edit-close"></i>
-            <div class="u-mgb--10">
-              <a href="" class="c-button --edit">編集</a>
-            </div>
-            <form action="" method="POST">
-              <button class="c-button --delete">削除</button>
-            </form>
-          </div>
-        </div> --}}
-
       </div>
 
 
