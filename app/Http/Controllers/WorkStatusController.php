6e86210ab8ec14b6ad1809d4acaf3a54f25ecc20
github yuki-shimu-coder <yuyuyou7@gmail.com>
2022-02-21 19:54:38 +0900

    超勤状況確認コントローラーの修正
    >時間計算関数を共通関数に変更(private)
    >超勤総合時間の算出用とページネーション用の処理をそれぞれ実装
    >特定年で超勤記録データを取得するメソッドを用意（未実装）
    >viewを編集（超勤時間の総計を表示、記録なしの表示を追加、ページネーションリンクの追加）

--- app/Http/Controllers/WorkStatusController.php
+++ app/Http/Controllers/WorkStatusController.php
@@ -13,43 +13,46 @@ use App\Http\Requests\WorkTimeRecordPost;
 
 class WorkStatusController extends Controller
 {
+    // ======================================================
+    // 時差計算関数(共通)
+    // ======================================================
+    private function diffTime($record_date, $start_time, $end_time)
+    {
+        // カーボンオブジェクトを使用して、時差を計算する
+        // カーボンにより開始時刻を新たに定義
+        $start_time_carbon = new Carbon($record_date . $start_time);
+
+        // カーボンにより終了時刻を新たに定義
+        $end_time_carbon = new Carbon($record_date . $end_time);
+
+        // 開始時間が終了時間より大きい場合　…　つまり日をまたいだ場合
+        if ($start_time > $end_time) {
+            // 終了時刻の日付を１日加算する
+            $end_time_carbon->addDay();
+        }
+
+        // 分単位の時間差を算出する
+        $diff_minute_carbon = $start_time_carbon->diffInMinutes($end_time_carbon);
+
+        // 算出した値を返す
+        return $diff_minute_carbon;
+    }
+
+    // ======================================================
     // ログインユーザーの超勤状況を取得し、一覧表示する
+    // ======================================================
     public function show()
     {
         // ログインユーザー名を取得する
-        $user = Auth::user();
-        $user_name = $user->name;
+        $user_name = Auth::user()->name;
 
-        //超勤状況を取得するために、モデルを用意
-        // 申請日が最新のものから順に取得する（降順）
+        // ======================================================
+        // 超勤時間総計用
+        // ======================================================
         $work_status = Auth::user()->worktime_records()->orderBy('record_date', 'asc')->get();
 
-        // 時差計算関数
-        function diffTime($record_date, $start_time, $end_time)
-        {
-            // カーボンオブジェクトを使用して、時差を計算する
-            // カーボンにより開始時刻を新たに定義
-            $start_time_carbon = new Carbon($record_date . $start_time);
-
-            // カーボンにより終了時刻を新たに定義
-            $end_time_carbon = new Carbon($record_date . $end_time);
-
-            // 開始時間が終了時間より大きい場合　…　つまり日をまたいだ場合
-            if ($start_time > $end_time) {
-                // 終了時刻の日付を１日加算する
-                $end_time_carbon->addDay();
-            }
-
-            // 分単位の時間差を算出する
-            $diff_minute_carbon = $start_time_carbon->diffInMinutes($end_time_carbon);
-
-            // 算出した値を返す
-
-            return $diff_minute_carbon;
-            // $total += $diff_minute_carbon;
-        }
-
-
+        // 合計時間格納用の変数
+        $total = 0;
 
         // 取得した情報から超勤時間を算出する
         foreach ($work_status as $key => $value) {
@@ -64,7 +67,7 @@ class WorkStatusController extends Controller
             // 平日早朝　終了時刻
             $weekday_morning_end = $value['weekday_morning_end'];
             // 平日早朝の超勤時間
-            $diff_minute_weekday_morning = diffTime($record_date, $weekday_morning_start, $weekday_morning_end);
+            $diff_minute_weekday_morning = $this->diffTime($record_date, $weekday_morning_start, $weekday_morning_end);
 
             // ======================================================
             // 平日一般
@@ -74,7 +77,7 @@ class WorkStatusController extends Controller
             // 平日一般　終了時刻
             $weekday_normal_end = $value['weekday_normal_end'];
             // 平日一般の超勤時間
-            $diff_minute_weekday_normal = diffTime($record_date, $weekday_normal_start, $weekday_normal_end);
+            $diff_minute_weekday_normal = $this->diffTime($record_date, $weekday_normal_start, $weekday_normal_end);
 
             // ======================================================
             // 平日深夜
@@ -84,7 +87,7 @@ class WorkStatusController extends Controller
             // 平日深夜　終了時刻
             $weekday_midnight_end = $value['weekday_midnight_end'];
             // 平日深夜の超勤時間
-            $diff_minute_weekday_midnight = diffTime($record_date, $weekday_midnight_start, $weekday_midnight_end);
+            $diff_minute_weekday_midnight = $this->diffTime($record_date, $weekday_midnight_start, $weekday_midnight_end);
 
             // ======================================================
             // 休日一般
@@ -94,7 +97,7 @@ class WorkStatusController extends Controller
             // 休日一般　終了時刻
             $holiday_end = $value['holiday_end'];
             // 休日一般の超勤時間
-            $diff_minute_holiday = diffTime($record_date, $holiday_start, $holiday_end);
+            $diff_minute_holiday = $this->diffTime($record_date, $holiday_start, $holiday_end);
 
             // ======================================================
             // 休日深夜
@@ -104,44 +107,104 @@ class WorkStatusController extends Controller
             // 休日深夜　終了時刻
             $holiday_midnight_end = $value['holiday_midnight_end'];
             // 休日深夜の超勤時間
-            $diff_minute_holiday_midnight = diffTime($record_date, $holiday_midnight_start, $holiday_midnight_end);
+            $diff_minute_holiday_midnight = $this->diffTime($record_date, $holiday_midnight_start, $holiday_midnight_end);
 
-
-
-            // 平日早朝の超勤時間を配列に追加する
-            // $work_status[$key]['diff_minute_weekday_morning'] = $diff_minute_weekday_morning;
-
-            // 平日一般の超勤時間を配列に追加する
-            // $work_status[$key]['diff_minute_weekday_normal'] = $diff_minute_weekday_normal;
-
-            // 平日深夜の超勤時間を配列に追加する
-            // $work_status[$key]['diff_minute_weekday_midnight'] = $diff_minute_weekday_midnight;
-
-            // 休日一般の超勤時間を配列に追加する
-            // $work_status[$key]['diff_minute_holiday'] = $diff_minute_holiday;
-
-            //  休日一般の超勤時間を配列に追加する
-            // $work_status[$key]['diff_minute_holiday_midnight'] = $diff_minute_holiday_midnight;
-
+            // ======================================================
             // 1レコードの勤務時間を合計して、申請日の超勤時間を算出する
+            // ======================================================
             $oneday_worktime = $diff_minute_weekday_morning + $diff_minute_weekday_normal + $diff_minute_weekday_midnight + $diff_minute_holiday + $diff_minute_holiday_midnight;
 
-            $work_status[$key]['oneday_worktime'] = $oneday_worktime;
+            // 超勤総計時間を算出
+            $total += $oneday_worktime;
+        }
+
+        // 時間単位と分単位に変換
+        $total_worktime_hour = (int)floor($total / 60); //floorではfloat型を返すのでint型にキャスト
+        $total_worktime_minute = $total % 60;
+
+
+        // ======================================================
+        // ページネーション用
+        // ======================================================
+        $work_status_pagination = Auth::user()->worktime_records()->orderBy('record_date', 'asc')->paginate(10);
+
+        // 取得した情報から超勤時間を算出する
+        foreach ($work_status_pagination as $key => $value) {
+            // 申請日
+            $record_date = $value['record_date'];
+
+            // ======================================================
+            // 平日早朝
+            // ======================================================
+            // 平日早朝　開始時刻
+            $weekday_morning_start = $value['weekday_morning_start'];
+            // 平日早朝　終了時刻
+            $weekday_morning_end = $value['weekday_morning_end'];
+            // 平日早朝の超勤時間
+            $diff_minute_weekday_morning = $this->diffTime($record_date, $weekday_morning_start, $weekday_morning_end);
+
+            // ======================================================
+            // 平日一般
+            // ======================================================
+            // 平日一般　開始時刻
+            $weekday_normal_start = $value['weekday_normal_start'];
+            // 平日一般　終了時刻
+            $weekday_normal_end = $value['weekday_normal_end'];
+            // 平日一般の超勤時間
+            $diff_minute_weekday_normal = $this->diffTime($record_date, $weekday_normal_start, $weekday_normal_end);
+
+            // ======================================================
+            // 平日深夜
+            // ======================================================
+            // 平日深夜　開始時刻
+            $weekday_midnight_start = $value['weekday_midnight_start'];
+            // 平日深夜　終了時刻
+            $weekday_midnight_end = $value['weekday_midnight_end'];
+            // 平日深夜の超勤時間
+            $diff_minute_weekday_midnight = $this->diffTime($record_date, $weekday_midnight_start, $weekday_midnight_end);
+
+            // ======================================================
+            // 休日一般
+            // ======================================================
+            // 休日一般　開始時刻
+            $holiday_start = $value['holiday_start'];
+            // 休日一般　終了時刻
+            $holiday_end = $value['holiday_end'];
+            // 休日一般の超勤時間
+            $diff_minute_holiday = $this->diffTime($record_date, $holiday_start, $holiday_end);
+
+            // ======================================================
+            // 休日深夜
+            // ======================================================
+            // 休日深夜　開始時刻
+            $holiday_midnight_start = $value['holiday_midnight_start'];
+            // 休日深夜　終了時刻
+            $holiday_midnight_end = $value['holiday_midnight_end'];
+            // 休日深夜の超勤時間
+            $diff_minute_holiday_midnight = $this->diffTime($record_date, $holiday_midnight_start, $holiday_midnight_end);
+
+            // ======================================================
+            // 1レコードの勤務時間を合計して、申請日の超勤時間を算出する
+            // ======================================================
+            $oneday_worktime = $diff_minute_weekday_morning + $diff_minute_weekday_normal + $diff_minute_weekday_midnight + $diff_minute_holiday + $diff_minute_holiday_midnight;
 
             // 時間単位と分単位に変換
             $oneday_worktime_hour = (int)floor($oneday_worktime / 60); //floorではfloat型を返すのでint型にキャスト
             $oneday_worktime_minute = $oneday_worktime % 60;
 
-            $work_status[$key]['oneday_worktime_hour'] = $oneday_worktime_hour;
-            $work_status[$key]['oneday_worktime_minute'] = $oneday_worktime_minute;
+            // 特定の１日の超勤時間（時間）
+            $work_status_pagination[$key]['oneday_worktime_hour'] = $oneday_worktime_hour;
+
+            // 特定の１日の超勤時間（分）
+            $work_status_pagination[$key]['oneday_worktime_minute'] = $oneday_worktime_minute;
         }
 
-        // dd($work_status);
-
-        return view('template_self.workstatus', compact('user_name', 'work_status'));
+        return view('template_self.workstatus', compact('user_name', 'work_status', 'work_status_pagination', 'total', 'total_worktime_hour', 'total_worktime_minute'));
     }
 
+    // ======================================================
     // 超勤一覧から任意の記録を編集する
+    // ======================================================
     public function edit($id)
     {
         // GETパラメータが数字かどうかをチェックする
@@ -178,7 +241,9 @@ class WorkStatusController extends Controller
         return view('template_self.worktime-edit', compact('worktimeRecord', 'weekday_morning_start', 'weekday_morning_end', 'weekday_normal_start', 'weekday_normal_end', 'weekday_midnight_start', 'weekday_midnight_end', 'holiday_start', 'holiday_end', 'holiday_midnight_start', 'holiday_midnight_end'));
     }
 
+    // ======================================================
     // 超勤記録を更新する
+    // ======================================================
     public function update(WorkTimeRecordPost $request, $id)
     {
         // GETパラメータが数字かどうかをチェックする
@@ -199,7 +264,9 @@ class WorkStatusController extends Controller
         return redirect('/workstatus/mypage')->with('flash_message', '超勤を更新しました');
     }
 
+    // ======================================================
     // 超勤記録を削除する
+    // ======================================================
     public function destroy($id)
     {
         // GETパラメータが数字かどうかをチェックする
@@ -212,5 +279,123 @@ class WorkStatusController extends Controller
         Auth::user()->worktime_records()->find($id)->delete();
 
         return redirect('/workstatus/mypage')->with('flash_message', '記録を削除しました');
+    }
+
+    // ======================================================
+    // 特定年で超勤記録データを取得する
+    // ======================================================
+    public function getYearWork()
+    {
+        // ログインユーザー名を取得する
+        $user_name = Auth::user()->name;
+
+        //超勤状況を取得するために、モデルを用意
+        // 申請日が最新のものから順に取得する（降順）
+        $work_status = Auth::user()->worktime_records()->orderBy('record_date', 'asc')->whereYear('record_date', '2022')->get();
+
+
+        // 合計時間格納用の変数
+        $total = 0;
+
+        // 取得した情報から超勤時間を算出する
+        foreach ($work_status as $key => $value) {
+            // 申請日
+            $record_date = $value['record_date'];
+
+            // ======================================================
+            // 平日早朝
+            // ======================================================
+            // 平日早朝　開始時刻
+            $weekday_morning_start = $value['weekday_morning_start'];
+            // 平日早朝　終了時刻
+            $weekday_morning_end = $value['weekday_morning_end'];
+            // 平日早朝の超勤時間
+            $diff_minute_weekday_morning = $this->diffTime($record_date, $weekday_morning_start, $weekday_morning_end);
+
+            // ======================================================
+            // 平日一般
+            // ======================================================
+            // 平日一般　開始時刻
+            $weekday_normal_start = $value['weekday_normal_start'];
+            // 平日一般　終了時刻
+            $weekday_normal_end = $value['weekday_normal_end'];
+            // 平日一般の超勤時間
+            $diff_minute_weekday_normal = $this->diffTime($record_date, $weekday_normal_start, $weekday_normal_end);
+
+            // ======================================================
+            // 平日深夜
+            // ======================================================
+            // 平日深夜　開始時刻
+            $weekday_midnight_start = $value['weekday_midnight_start'];
+            // 平日深夜　終了時刻
+            $weekday_midnight_end = $value['weekday_midnight_end'];
+            // 平日深夜の超勤時間
+            $diff_minute_weekday_midnight = $this->diffTime($record_date, $weekday_midnight_start, $weekday_midnight_end);
+
+            // ======================================================
+            // 休日一般
+            // ======================================================
+            // 休日一般　開始時刻
+            $holiday_start = $value['holiday_start'];
+            // 休日一般　終了時刻
+            $holiday_end = $value['holiday_end'];
+            // 休日一般の超勤時間
+            $diff_minute_holiday = $this->diffTime($record_date, $holiday_start, $holiday_end);
+
+            // ======================================================
+            // 休日深夜
+            // ======================================================
+            // 休日深夜　開始時刻
+            $holiday_midnight_start = $value['holiday_midnight_start'];
+            // 休日深夜　終了時刻
+            $holiday_midnight_end = $value['holiday_midnight_end'];
+            // 休日深夜の超勤時間
+            $diff_minute_holiday_midnight = $this->diffTime($record_date, $holiday_midnight_start, $holiday_midnight_end);
+
+
+            // 平日早朝の超勤時間を配列に追加する
+            // $work_status[$key]['diff_minute_weekday_morning'] = $diff_minute_weekday_morning;
+
+            // 平日一般の超勤時間を配列に追加する
+            // $work_status[$key]['diff_minute_weekday_normal'] = $diff_minute_weekday_normal;
+
+            // 平日深夜の超勤時間を配列に追加する
+            // $work_status[$key]['diff_minute_weekday_midnight'] = $diff_minute_weekday_midnight;
+
+            // 休日一般の超勤時間を配列に追加する
+            // $work_status[$key]['diff_minute_holiday'] = $diff_minute_holiday;
+
+            //  休日一般の超勤時間を配列に追加する
+            // $work_status[$key]['diff_minute_holiday_midnight'] = $diff_minute_holiday_midnight;
+
+            // 1レコードの勤務時間を合計して、申請日の超勤時間を算出する
+            $oneday_worktime = $diff_minute_weekday_morning + $diff_minute_weekday_normal + $diff_minute_weekday_midnight + $diff_minute_holiday + $diff_minute_holiday_midnight;
+
+            // 超勤総計時間を算出
+            $total += $oneday_worktime;
+
+            // dd($total);
+
+            // // 特定の１日の超勤時間（分単位）
+            // $work_status[$key]['oneday_worktime'] = $oneday_worktime;
+
+            // 時間単位と分単位に変換
+            $oneday_worktime_hour = (int)floor($oneday_worktime / 60); //floorではfloat型を返すのでint型にキャスト
+            $oneday_worktime_minute = $oneday_worktime % 60;
+
+            // 特定の１日の超勤時間（時間）
+            $work_status[$key]['oneday_worktime_hour'] = $oneday_worktime_hour;
+
+            // 特定の１日の超勤時間（分）
+            $work_status[$key]['oneday_worktime_minute'] = $oneday_worktime_minute;
+        }
+
+        // 時間単位と分単位に変換
+        $total_worktime_hour = (int)floor($total / 60); //floorではfloat型を返すのでint型にキャスト
+        $total_worktime_minute = $total % 60;
+
+
+        // dd($work_status);
+        return view('template_self.workstatus', compact('user_name', 'work_status', 'total_worktime_hour', 'total_worktime_minute'));
     }
 }
--- resources/views/template_self/workstatus.blade.php
+++ resources/views/template_self/workstatus.blade.php
@@ -5,7 +5,19 @@
 @section('content')
 <div class="l-container">
   <div class="p-work-status">
-    <h1 class="p-work-status__title u-mgb--40">{{$user_name}}さんの超勤状況</h1>
+    <div class="p-work-status__top-wrapper">
+      <div>
+        <h1 class="p-work-status__title">{{$user_name}}さんの超勤状況</h1>
+      </div>
+      <div class="p-work-status__total-time">
+        <p>超勤時間の総計</p>
+        <span>{{ $total_worktime_hour}}</span>時間
+        <div style="display:inline;margin-right: 5px;"></div>
+        <span>{{$total_worktime_minute}}</span>分
+      </div>
+    </div>
+
+
     {{-- 超勤状況一覧 --}}
     <section class="p-work-status__history">
       <div class="p-work-status__history-head">
@@ -13,50 +25,31 @@
       </div>
       <div class="p-work-status__history-body">
 
-        @foreach ($work_status as $value)
-        {{-- <div class="p-work-status__history-contents">
-          <time class="p-work-status__history-date" datetime="{{$value->record_date}}">{{$value->record_date}}</time>
+        {{-- 超勤時間の記録がないとき --}}
+        @if (empty($total))
 
+        まだ記録がありません。
 
-          <div class="p-work-status__history-worktime">
-            <span>{{ $value->oneday_worktime_hour }}</span>時間
-            <div style="display:inline;margin-right: 5px;"></div>
-            <span>{{ $value->oneday_worktime_minute }}</span>分
-          </div>
+        @else
 
+        @foreach ($work_status_pagination as $value)
 
-          <div class="p-work-status__history-subject">
-            {{$value->work_content}}
-          </div>
-          <div class="p-work-status__history-edit-trigger">
-            <i class="fas fa-ellipsis-h"></i>
-          </div>
-          <div class="p-work-status__history-edit-wrap">
-
-            <i class="fas fa-times-circle p-work-status__history-edit-close" style="color: #fff;"></i>
-            <div class="u-mgb--10">
-              <a href="" class="c-button --edit">編集</a>
-            </div>
-            <form action="" method="POST">
-              @csrf
-              <button class="c-button --delete">削除</button>
-            </form>
-          </div>
-        </div> --}}
-
+        {{-- vueコンポーネントで超勤記録時間の一覧表示 --}}
         <workstatus-component record_date="{{$value->record_date}}"
           oneday_worktime_hour="{{$value->oneday_worktime_hour}}"
-          oneday_worktime_minute="{{$value->oneday_worktime_minute}}"
-          work_content="{{$value->work_content}}"
-          v-bind:csrf="{{ json_encode(csrf_token()) }}"
-          edit="{{ route('worktime-edit', ['id' => $value->id]) }}"
+          oneday_worktime_minute="{{$value->oneday_worktime_minute}}" work_content="{{$value->work_content}}"
+          v-bind:csrf="{{ json_encode(csrf_token()) }}" edit="{{ route('worktime-edit', ['id' => $value->id]) }}"
           destroy="{{ route('worktime-delete', ['id' => $value->id]) }}"></workstatus-component>
+
         @endforeach
 
+        {{ $work_status_pagination->links() }}
+
+        @endif
       </div>
-
-
     </section>
+
+
 
   </div>
 </div>
